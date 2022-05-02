bebd4ec7b6f921d0bb1e2624360bb3cd078c2561
Esenushka <prosterling.mien@gmail.com>
2022-05-02 13:44:40 +0600

    add main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/card';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const data = new FormData()
+    data.append("actionName", "MessagesLoad")
+    axios.post("http://f0665380.xsph.ru/", data)
+      .then(res => {
+        const sortedMessages = res.data.Messages.sort((a, b) => (
+          new Date(a.date).getTime() - new Date(b.date).getTime()
+        ))
+        setData(sortedMessages)
+      })
+  }, [])
+
+  const reverse = (e) => {
+    const rev = data.reverse();
+    if (e.target.value) {
+      return setData([...rev])
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div onChange={(e) => reverse(e)} className='radio_wrapper'>
+        <label >
+          <input name='radio' className='radio' type={"radio"} />
+          Новые сверху
+        </label>
+        <label>
+          <input defaultChecked name='radio' className='radio' type={"radio"} />
+          Новые снизу
+        </label>
+      </div>
+      {
+        data.map((el) => <Card key={el.id} {...el} />)
+      }
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react"
+
+export default function Card({ author, channel, date, content, attachments, id }) {
+    const stars = JSON.parse(localStorage.getItem("star"))
+    const [active, setActive] = useState(stars[id])
+    const [longStr, setLongStr] = useState(content.length > 250 ? true : false)
+    const [hideShow, setHideShow] = useState(false)
+    const add = () => {
+        setActive(!active)
+    }
+    useEffect(() => {
+        localStorage.setItem("star", JSON.stringify({ ...stars, [id]: active }))
+    }, [active])
+    return (
+        <div className="main_wrapper">
+            <header>
+                <div className='left'>
+                    <img src='/images/photo.png' alt='photo' />
+                    <div>
+                        <div>
+                            {author}
+                        </div>
+                        <div>
+                            {channel}
+                        </div>
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className='buttons'>
+                        <button className='btn'>Левый</button>
+                        <button className='btn'>Центр</button>
+                        <button className='btn'>Правый</button>
+                    </div>
+                    <div className='icons'>
+                        <img src='/images/send.png' alt='send_icon' />
+                        <img title={hideShow ? "Показать" : "Скрыть"} onClick={() => { setHideShow(!hideShow) }} src='/images/hide.png' alt='hide_icon' />
+                        <img src='/images/setting.png' alt='setting_icon' />
+                        <img title="Избранное" onClick={add} className={`${active ? "active" : ""}`} src='/images/star.png' alt='star_icon' />
+                    </div>
+                </div>
+            </header>
+            <main >
+                <div>{date}</div>
+                <div>
+                    <div className={hideShow ? "active" : ""}>
+                        {longStr ? content.slice(0, 250) + " . . ." : content}
+                    </div>
+                    {
+                        longStr ? <div className={hideShow ? "active" : ""} onClick={() => { setLongStr(false) }}>Далее</div> : ""
+                    }
+
+                    {
+                        attachments.map((el) =>
+                            el.type === "video" ?
+                                <video key={el.type} className="attachment" controls width={500} height={250} >
+                                    <source type="video/mp4" src={el.url}>
+                                    </source>
+                                </video>
+                                :
+                                <img key={el.type} className="attachment" src={el.url} />
+                        )
+                    }
+                </div>
+            </main>
+            <div className="tag">
+                <div className='active'>#Новое</div>
+                <div>#Эксперт</div>
+            </div>
+        </div>
+    )
+}
